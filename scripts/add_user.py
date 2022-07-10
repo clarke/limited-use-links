efe05144f5eac8cae8f8eab98279429f340aedfe
Clarke Retzer <clarke@carfoo.com>
2022-07-09 19:35:04 -0500

    Adding a script to add a user

---
+++ scripts/add_user.py
@@ -0,0 +1,17 @@
+from app import db
+from app.mod_auth.models import User
+from getpass import getpass
+import hashlib
+
+
+username = input("Enter username:")
+email = input("Enter email:")
+password = getpass("Password:")
+
+if username is not None and email is not None and password is not None:
+    hashed_password = hashlib.sha512(password.encode('utf-8')).hexdigest()
+    u = User(username=username, email=email, password=hashed_password)
+    db.session.add(u)
+    db.session.commit()
+else:
+    print("One of the values was empty")
